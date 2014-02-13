012dea326980fe4e573967ec3f07530dfcbc863e
nvzver <nv.zver@gmail.com>
2014-02-13 15:46:12 +0600

    First commit. Добавлен проект MyMediaPlayer с использованием DirectShow

---
+++ MyMediaPlayer/MyMediaPlayer/Media.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using QuartzTypeLib;
+using TestTrackBar.Controls;
+
+namespace MyMediaPlayer
+{
+   public class Media
+    {
+        public Media()
+        {
+        }
+
+        public enum mStatus{Play, Stop, Pause}
+        public mStatus currentStatus;
+
+        private const int WS_CHILD = 0x40000000;
+        private const int WS_CLIPCHILDREN = 0x2000000;
+
+        private FilgraphManager graphManager = new FilgraphManager();
+
+        private IVideoWindow mWindow = null;
+        private IMediaControl mControl = null;
+        public IMediaPosition mPosition = null;
+        private IMediaEvent mEvent;
+
+       public int second1;
+       public int ctlTrackPosition;
+
+       public bool loadFile(string sfile, Panel vPanel)
+        {
+            graphManager.RenderFile(sfile);
+            mWindow = graphManager as IVideoWindow;
+            mControl = graphManager as IMediaControl;
+            mPosition = graphManager as IMediaPosition;
+            
+            mWindow.Owner = (int) vPanel.Handle;
+            mWindow.WindowStyle = WS_CHILD | WS_CLIPCHILDREN;
+            mWindow.SetWindowPosition(vPanel.ClientRectangle.Left,
+                                       vPanel.ClientRectangle.Top,
+                                       vPanel.ClientRectangle.Width,
+                                       vPanel.ClientRectangle.Height);
+            //=====
+            
+
+            //=====
+            currentStatus = mStatus.Play;
+            second1 = (int) mPosition.Duration;
+            ctlTrackPosition = second1;
+            mControl.Run();
+
+            return true;
+        }
+
+        public bool playMedia()
+        {
+           
+            currentStatus = mStatus.Play;
+            mControl.Run();
+            return true;
+        }
+
+        public bool pauseMedia()
+        {
+            currentStatus = mStatus.Pause;
+            mControl.Pause();
+            return true;
+        }
+
+        public bool stopMedia()
+        {
+            currentStatus = mStatus.Stop;
+            mControl.Stop();
+            mPosition.CurrentPosition = 0;
+            return true;
+        }
+
+        public void mVoilum()
+        {
+            
+        }
+
+       public void TimeManager(Label time1)
+        {
+           if (time1.Text == "00:00:00")
+           {
+               second1 = (int)mPosition.Duration;
+           }
+
+           if (mPosition!=null)
+            {
+            int hour = second1/3600;
+            int minute = (second1 - (hour*3600))/60;
+            int second = second1-(hour*3600+minute*60);
+            time1.Text = String.Format("{0:D2}:{1:D2}:{2:D2}", hour, minute, second);
+            second1 -= 1;
+            }
+            else
+            {
+                time1.Text = "00:00:00";
+            }
+            
+        }
+
+       public void trbMedia(TrackBar trBar)
+       {
+          trBar.Value = ((int)mPosition.CurrentPosition * 100) / ctlTrackPosition;
+       }
+
+       public void trbScrollMedia(double sec)
+       {
+           mPosition.CurrentPosition = (double)(ctlTrackPosition*sec/100);
+       }
+    }
+}
---
+++ MyMediaPlayer/MyMediaPlayer/frmMain.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MyMediaPlayer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tmrVideo = new System.Windows.Forms.Timer(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlMedia = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(282, 473);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(70, 20);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(358, 473);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(70, 20);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(434, 473);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(70, 20);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tmrVideo
+            // 
+            this.tmrVideo.Interval = 1000;
+            this.tmrVideo.Tick += new System.EventHandler(this.tmrVideo_Tick);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(201, 473);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 20);
+            this.btnOpen.TabIndex = 13;
+            this.btnOpen.Text = "Open...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(15, 527);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 15;
+            // 
+            // pnlMedia
+            // 
+            this.pnlMedia.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pnlMedia.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlMedia.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMedia.Location = new System.Drawing.Point(0, 0);
+            this.pnlMedia.Name = "pnlMedia";
+            this.pnlMedia.Size = new System.Drawing.Size(704, 422);
+            this.pnlMedia.TabIndex = 17;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(0, 425);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(704, 42);
+            this.trackBar1.TabIndex = 19;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 549);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pnlMedia);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "frmMain";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer tmrVideo;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlMedia;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
